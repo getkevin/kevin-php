f2c48ba30c6bde452df198ad46488c1d88348904
saltanovas <zumbass@gmail.com>
2021-10-18 12:42:57 +0300

    Requested changes on SecurityManager class

--- src/SecurityManager.php
+++ src/SecurityManager.php
@@ -5,54 +5,28 @@ namespace Kevin;
 class SecurityManager
 {
     /**
-     * Endpoint secret.
-     *
-     * @var string
-     */
-    private $endpointSecret;
-
-    /**
-     * SecurityManager constructor.
-     *
-     * @param string $endpointSecret
-     */
-    public function __construct($endpointSecret = '')
-    {
-        $this->endpointSecret = $endpointSecret;
-    }
-
-    /**
      * An implementation of kevin.'s signature.
      * @see https://docs.kevin.eu/public/platform/v0.3#tag/Signature
      *
+     * @param string $endpointSecret
      * @param string $requestBody
      * @param array $headers
      * @param string $webhookUrl
-     * @param float $timestampTimeout in milliseconds
+     * @param int|null $timestampTimeout in milliseconds
      * @return bool
      */
-    public function verifySignature($requestBody, $headers, $webhookUrl, $timestampTimeout = null)
+    public static function verifySignature($endpointSecret, $requestBody, $headers, $webhookUrl, $timestampTimeout = null)
     {
-        if (!$this->verifyTimeout($timestampTimeout, $headers)) {
+        if (!self::verifyTimeout($timestampTimeout, $headers)) {
             return false;
         }
 
+        print_r("good\n");
+
         $data = 'POST' . $webhookUrl . $headers['X-Kevin-Timestamp'] . $requestBody;
-        $signature = $this->generateHasWithHmac256($data, $this->endpointSecret);
+        $signature = hash_hmac('sha256', $data, $endpointSecret);
 
-        return $signature == $headers['X-Kevin-Signature'];
-    }
-
-    /**
-     * Generate HMAC SHA256 hash.
-     *
-     * @param string $data
-     * @param string $key
-     * @return string
-     */
-    private function generateHasWithHmac256($data, $key)
-    {
-        return hash_hmac('sha256', $data, $key);
+        return $signature === $headers['X-Kevin-Signature'];
     }
 
     /**
@@ -62,17 +36,17 @@ class SecurityManager
      * @param array $headers
      * @return bool
      */
-    private function verifyTimeout($timestampTimeout, $headers)
+    private static function verifyTimeout($timestampTimeout, $headers)
     {
         if (!isset($headers['X-Kevin-Timestamp'])) {
             return false;
         }
 
-        if (!isset($timestampTimeout)) {
+        if ($timestampTimeout === null) {
             return true;
         }
 
-        $timeDifference = (microtime(true) * 1000) - $headers['X-Kevin-Timestamp'];
+        $timeDifference = (time() * 1000) - $headers['X-Kevin-Timestamp'];
 
         return $timestampTimeout > $timeDifference;
     }
